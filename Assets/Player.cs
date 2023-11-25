751e86b6c4df803fc3294035e820ef517b46f5be
Felipe Sousa <felipevsousa7@gmail.com>
2023-11-25 04:02:06 -0300

    Organizando e Animando corretamente

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -9,8 +9,9 @@ public class Player : MonoBehaviour
 
     [SerializeField] private float moveSpeed;
     [SerializeField] private float jumpForce;
-    [SerializeField] bool isMoving;
     private float xInput;
+    private int facingDirection = 1;
+    private bool facingRight = true;
 
     // Start is called before the first frame update
     void Start()
@@ -22,17 +23,42 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        xInput = Input.GetAxisRaw("Horizontal");
+        Movement();
+        CheckInput();
+        AnimatorController();
+        FlipController();
+    }
 
+    private void Movement(){
         rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
+    }
 
+    private void Jump(){
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+    }
+
+    private void CheckInput(){
+        xInput = Input.GetAxisRaw("Horizontal");
         if (Input.GetKeyDown(KeyCode.Space))
-        {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-        }
+            Jump();
+    }
 
-        isMoving = rb.velocity.x != 0;
+        private void AnimatorController() {
+        bool isMoving = rb.velocity.x != 0;
 
         anim.SetBool("isMoving",isMoving);
+    }
+
+    private void Flip(){
+        facingDirection = facingDirection *-1;
+        facingRight = !facingRight;
+        transform.Rotate(0, 180, 0);
+    }
+
+    private void FlipController(){
+        if(rb.velocity.x > 0 && !facingRight)
+            Flip();
+        else if(rb.velocity.x < 0 && facingRight)
+            Flip();
     }
 }
