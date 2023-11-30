3f4448c8c19fa7a5a28b15f3e7c1040f2e472a4d
Felipe Vinicius de Sousa <31699547+benzerinsio@users.noreply.github.com>
2023-11-30 02:21:47 -0300

    Dash Feature and Attack Animation

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,48 +1,68 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField]
-    private float moveSpeed;
-
-    [SerializeField]
-    private float jumpForce;
     private Rigidbody2D rb;
     private Animator anim;
+
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
+
+    [Header("Dash Info")]
+    [SerializeField] private float dashSpeed;
+    [SerializeField] private float dashDuration;
+    private float dashTime;
+
+    [SerializeReference] private float dashCooldown;
+    private float dashCooldownTimer;
+
+    [Header("Attack Info")]
+    private bool isAttacking;
+    private int comboCounter;
+
     private float xInput;
+
     private int facingDirection = 1;
     private bool facingRight = true;
 
     [Header("Collision Info")] //doesn't work if private value is right after the header
-    [SerializeField]
-    private float groundCheckDistance;
-
-    [SerializeField]
-    private LayerMask whatIsGround;
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask whatIsGround;
     private bool isGrounded;
 
-    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponentInChildren<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         Movement();
         CheckInput();
         CollisionChecks();
+
+        dashTime -= Time.deltaTime;
+        dashCooldownTimer -= Time.deltaTime;
+
         FlipController();
         AnimatorController();
     }
 
+    public void AttackOver()
+    {
+        isAttacking = false;
+    }
     private void Movement()
     {
-        rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
+        if (dashTime > 0)
+        {
+            rb.velocity = new Vector2(xInput * dashSpeed, 0);
+        }
+        else
+        {
+            rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
+        }
     }
 
     private void Jump()
@@ -51,13 +71,31 @@ public class Player : MonoBehaviour
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
     }
 
-    private void CheckInput()
+    private void CheckInput() //only handle the input (don't do a lot of check here)
     {
         xInput = Input.GetAxisRaw("Horizontal");
         if (Input.GetKeyDown(KeyCode.Space))
             Jump();
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            dashAbility();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            isAttacking = true;
+        }
     }
 
+    private void dashAbility()
+    {
+        if (dashCooldownTimer < 0)//the second part of input check (the first part is only input as mentioned) 
+        {
+            dashCooldownTimer = dashCooldown;
+            dashTime = dashDuration;
+        }
+    }
     private void AnimatorController()
     {
         bool isMoving = rb.velocity.x != 0;
@@ -65,6 +103,9 @@ public class Player : MonoBehaviour
         anim.SetBool("isMoving", isMoving);
         anim.SetBool("isGrounded", isGrounded);
         anim.SetFloat("yVelocity", rb.velocity.y);
+        anim.SetBool("isDashing", dashTime > 0);
+        anim.SetBool("isAttacking", isAttacking);
+        anim.SetInteger("comboCounter", comboCounter);
     }
 
     private void Flip()
